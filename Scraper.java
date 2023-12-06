8bb3decd5c2fb4ace82d753cab53cb5197345928
TrevorEngland <trevor.england@stmartin.edu>
2023-12-06 13:30:16 -0800

    add dummy data

--- Scraper.java
+++ Scraper.java
@@ -4,6 +4,7 @@ import java.awt.*; // imports BorderLayout
 public class Scraper extends JFrame{
     // variables
     JTextField urlToGet;
+    JTable table;
 
     // methods
 
@@ -15,9 +16,16 @@ public class Scraper extends JFrame{
 
         setLayout(new BorderLayout());
 
-        urlToGet = new JTextField(); // initalizes 
+        urlToGet = new JTextField("Enter URL"); // initalizes 
         add(urlToGet, BorderLayout.NORTH); // adds to the JFrame
 
+        String columns[] = {"ID", "NAME", "SALARY"};
+        String data[][] = {{"1", "Trevor Engand", "Unemployed"}, {"2", "Amber England", "85000"}, {"3", "Bob Marley", "100000"}};
+
+        table = new JTable(data, columns); // intializes table
+        add(table); // adds to JFrame
+
+        // set JFrame visibility / viewing configs...
         setVisible(true);
         setSize(500,500);
         setLocationRelativeTo(null);
