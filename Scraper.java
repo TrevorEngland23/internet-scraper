f3977ae9de02c46cb8fa101650a9c09a209368ae
TrevorEngland <trevor.england@stmartin.edu>
2023-12-06 14:11:04 -0800

    Make datafields dynamic

--- Scraper.java
+++ Scraper.java
@@ -1,12 +1,17 @@
 import javax.swing.*;
+import javax.swing.table.*;
+
 import java.awt.*; // imports BorderLayout
+import java.awt.event.*;
 
 public class Scraper extends JFrame{
     // variables
     JTextField urlToGet;
+    DefaultTableModel tableModel;
     JTable table;
+    JTextField regexTextField;
+    JButton button;
 
-    // methods
 
     //ctor(s)
     public Scraper() {
@@ -19,17 +24,41 @@ public class Scraper extends JFrame{
         urlToGet = new JTextField("Enter URL"); // initalizes 
         add(urlToGet, BorderLayout.NORTH); // adds to the JFrame
 
-        String columns[] = {"ID", "NAME", "SALARY"};
-        String data[][] = {{"1", "Trevor Engand", "Unemployed"}, {"2", "Amber England", "85000"}, {"3", "Bob Marley", "100000"}};
+        // String columns[] = {"ID", "NAME", "SALARY"};
+        // String data[][] = {{"1", "Trevor Engand", "0"}, {"2", "Amber England", "85000"}, {"3", "Bob Marley", "100000"}};
 
-        table = new JTable(data, columns); // intializes table
-        JScrollPane scrollPane = new JScrollPane(table);
-        add(scrollPane);
+
+
+        tableModel = new DefaultTableModel(); // intializes table model
+        table = new JTable(tableModel); // initalizes JTable 
+
+        tableModel.addColumn("Line #"); // add line number column
+        tableModel.addColumn("Result"); // add result column
+
+        JScrollPane scrollPane = new JScrollPane(table); // adds a scrollpane to allow scrolling when data is too large for the JFrame
+        add(scrollPane); // adds the scrollPane to the JFrame
+
+        JPanel southJPanel = new JPanel(); // basically a "div", creates a section basically
+
+        regexTextField = new JTextField("Enter REGEX", 20); // instanciate a new text field... 20 is to make the bar on the bottom larger
+        southJPanel.add(regexTextField); // add it to the new section we just created.
+
+        button = new JButton("Submit");
+        button.addActionListener(e -> searchPage(e)); // lambda for action event
+        southJPanel.add(button);
+
+        add(southJPanel, BorderLayout.SOUTH); // append that to the bottom of the screen
 
         // set JFrame visibility / viewing configs...
         setVisible(true);
         setSize(500,500);
         setLocationRelativeTo(null);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
+    }
+
+    // methods
+
+    public void searchPage(ActionEvent e){
+        // todo
     }
 }
